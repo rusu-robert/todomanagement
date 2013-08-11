1843319bc1d10d92323f1e23045dcbfb2461cd0f
patriciaradu <patricia_radu@yahoo.com>
2013-08-11 20:01:28 +0300

    created new class List

---
+++ src/Modules/Todo/Models/List.cpp
@@ -0,0 +1,18 @@
+/*
+ * List.cpp
+ *
+ *  Created on: 11.08.2013
+ *      Author: Patri
+ */
+
+#include "List.h"
+
+List::List() {
+	// TODO Auto-generated constructor stub
+
+}
+
+List::~List() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Modules/Todo/Models/List.h
@@ -0,0 +1,17 @@
+/*
+ * List.h
+ *
+ *  Created on: 11.08.2013
+ *      Author: Patri
+ */
+
+#ifndef LIST_H_
+#define LIST_H_
+
+class List {
+public:
+	List();
+	virtual ~List();
+};
+
+#endif /* LIST_H_ */
